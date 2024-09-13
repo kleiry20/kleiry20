a8ad726257f63bccff5601ecefaba3d79e00b8a1
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-09-13 14:02:11 +0530

    working on project cards

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 function openProject(key) {
-  if (key == "crypto") {
-    window.open("https://intract-app.vercel.app/", "_blank");
+  //   if (key == "crypto") {
+  //     window.open("https://intract-app.vercel.app/", "_blank");
+  //   }
+  switch (key) {
+    case "crypto":
+      window.open("https://intract-app.vercel.app/", "_blank");
+      break;
+    case "event":
+      window.open("https://astrix-project-ten.vercel.app/", "_blank");
+      break;
+    case "weather":
+      window.open("https://weather-app-seven-ecru-91.vercel.app/", "_blank");
+      break;
+    case "apparel":
+      window.open("https://bewakoof-iat3.vercel.app/", "_blank");
+    default:
+      console.log("error routing the project");
   }
 }
