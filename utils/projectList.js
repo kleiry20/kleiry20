0fbbc38ee79be70f4da27ad90e7aa752918701fb
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-10-26 22:37:49 +0530

    fixing project data

---
+++ utils/projectList.js
@@ -0,0 +1,9 @@
+export const projectList = [
+  {
+    title: "Crypto Website",
+    description:
+      "This project replicates the UI of Intract's Academy tab. The page is divided into various sections consisting of uniquely designed components like cards and user journeys. Developed using ReactJs and CSS.",
+    skills: [],
+    link: "",
+  },
+];
