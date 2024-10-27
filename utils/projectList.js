45511624ad7a47882b32f1643b3e5fb1eb70d775
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-10-27 22:07:09 +0530

    data

--- utils/projectList.js
+++ utils/projectList.js
@@ -6,4 +6,11 @@ export const projectList = [
     skills: [],
     link: "",
   },
+  {
+    title: "Crypto Website",
+    description:
+      "This project replicates the UI of Intract's Academy tab. The page is divided into various sections consisting of uniquely designed components like cards and user journeys. Developed using ReactJs and CSS.",
+    skills: [],
+    link: "",
+  },
 ];
