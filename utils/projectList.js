495c6b249bbbb3bcb1e1ae70ebe880809e893ffc
kleiry20 <74005559+kleiry20@users.noreply.github.com>
2024-10-28 11:29:10 +0530

    fixing data

--- utils/projectList.js
+++ utils/projectList.js
@@ -3,14 +3,10 @@ export const projectList = [
     title: "Crypto Website",
     description:
       "This project replicates the UI of Intract's Academy tab. The page is divided into various sections consisting of uniquely designed components like cards and user journeys. Developed using ReactJs and CSS.",
-    skills: [],
-    link: "",
-  },
-  {
-    title: "Crypto Website",
-    description:
-      "This project replicates the UI of Intract's Academy tab. The page is divided into various sections consisting of uniquely designed components like cards and user journeys. Developed using ReactJs and CSS.",
-    skills: [],
-    link: "",
+    skills: [
+      
+    ],
+    link: "https://intract-app.vercel.app/",
+    
   },
 ];
